557a125d35efa099bb43a4118c07ecd272d6882a
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-03-08 23:56:31 +0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,29 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
-class Student(models.Model):
-    name=models.CharField()
-    email=models.EmailField()
-    age=models.IntegerField()
-    course=models.CharField()
-    created_at=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-# _________________________ Register Note ________________________
+# _________________________ Registeration Model ________________________
 class Register(AbstractUser):
     class Meta:
         verbose_name = "Register"          
         verbose_name_plural = "Register"
     
-class Note(models.Model):
-    user=models.ForeignKey(Register, on_delete=models.CASCADE)
-    title=models.CharField()
-    content=models.TextField()
-    created_at=models.DateTimeField(auto_now_add=True)
-# _________________ Product API ________________
+# _________________ Product API Model ________________
 class Category(models.Model):
     name=models.CharField()
 
